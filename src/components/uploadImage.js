44f1c68352ccbf1bd1f7965db53ffe512beccffc
alahaikal <haikalskak@gmail.com>
2024-02-23 08:08:45 +0700

    push fetch

--- src/components/uploadImage.js
+++ src/components/uploadImage.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function ImageUpload(){
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null)
   const [imagePreview, setImagePreview] = useState('')
-  const [imageUrl, setImageUrl] = useState('');
-  const [predictions, setPredictions] = useState([]);
+  const [predictedClass, setPredictedClass] = useState("")
+  const [probability, setProbability] = useState(0)
+  const [predictionTime, setPredictionTime] = useState(0)
 
   const getImage = (e) => {
     const file = e.target.files[0];
-    setImage(file); 
+    setImage(file)
     console.log(image)
     const URL_IMAGE = URL.createObjectURL(file)
-    setImagePreview(URL_IMAGE);
+    setImagePreview(URL_IMAGE)
   };
 
   const handleUpload = async (e) => {
     e.preventDefault()
-    const formData = new FormData();  //create new form object
-    formData.append("myImage", image); // myImage is variable that API want
-    
-    axios({
-      method: "post",
-      url: "http://localhost:5000/upload-image", // send to API
-      data: formData, 
-    })
-     .then((response) => {  
-      const { data } = response; //return image url of uploaded img
-      setImageUrl(data.url); //set url to image variable
-    })
-     .catch((err) => {
-      console.log(err);
-    });
-
+    const formData = new FormData()  //create new form object
+    formData.append("image", image) // myImage is variable that API want
+    console.log(image)
+    fetch("http://152.42.168.99/predict", {
+              method: "POST",
+              body: formData,
+          })
+              .then((response) => {
+                  return response.json()
+              })
+              .then((data) => {
+                  setPredictedClass(data.predicted_class)
+                  setProbability(data.probability)
+                  setPredictionTime(data.prediction_time)
+                  
+              });
 
   };
 
@@ -40,23 +41,30 @@ function ImageUpload(){
     <div>
       <h2>Clarifai Image Recognition</h2>
       <form onSubmit={handleUpload}>
-        <div>
-          <h3>Preview Gambar:</h3>
-          <img src={imagePreview} alt="Preview" style={{ maxWidth: '100%', maxHeight: '200px' }} />
-        </div>
         <input type="file" onChange={getImage} ></input>
         <button type="submit">upload</button>
       </form>
-      {predictions.length > 0 && (
-        <div>
-          <h3>Predictions:</h3>
-          <ul>
-            {predictions.map(prediction => (
-              <li key={prediction.id}>{prediction.name} - {prediction.value}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div>
+          <h3>Preview Gambar:</h3>
+          {imagePreview && (
+          <img
+            src={imagePreview}
+            alt="Preview"
+            style={{ maxWidth: '100%', maxHeight: '200px' }}
+          />
+        )}
+      </div>
+      <ul>
+        <li>
+          Prediction: {predictedClass}
+        </li>
+        <li>
+          Probability: {probability}
+        </li>
+        <li>
+          Prediction time: {predictionTime}
+        </li>
+      </ul>
     </div>
   )
 }
